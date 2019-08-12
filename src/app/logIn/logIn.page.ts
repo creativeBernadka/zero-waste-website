8c8615f12ae3207a818bd1090f45164cd5dc15e9
creativeBernadka <bfryczkowska@gmail.com>
2019-08-12 11:48:03 +0200

    ADD: login route added
    Login route and routing to it added.

---
+++ src/app/logIn/logIn.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-logIn',
+    templateUrl: 'logIn.page.html',
+    styleUrls: ['logIn.page.scss'],
+})
+export class LogInPage {
+
+    constructor() {
+    }
+}
