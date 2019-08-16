adb52d2110746c7052e40340f41f46152600c122
creativeBernadka <bfryczkowska@gmail.com>
2019-08-16 09:34:18 +0200

    ADD: authentication service added

--- src/app/logIn/logIn.page.ts
+++ src/app/logIn/logIn.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {AuthService} from '../../services/authentication.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-logIn',
     templateUrl: 'logIn.page.html',
     styleUrls: ['logIn.page.scss'],
+    providers: [AuthService]
 })
 export class LogInPage {
 
-    constructor() {
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+    ) {
+    }
+
+    login: string;
+    password: string;
+
+    verifyCredentials() {
+        console.log(this.login, this.password);
+        if (this.login && this.password) {
+            this.authService.login(this.login, this.password)
+                .subscribe(
+                    () => {
+                        console.log('User is logged in');
+                        this.router.navigateByUrl('/');
+                    }
+                );
+        }
     }
 }
