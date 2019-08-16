decf8c871d0d23e284d173c44cd5d67a7b0e58a8
creativeBernadka <bfryczkowska@gmail.com>
2019-08-16 09:38:14 +0200

    ADD: directory and files for user overview added
    directory /logIn is now /log_in. User overwiew is
    under /user_overview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'login', loadChildren: () => import('./logIn/logIn.module').then( m => m.LogInPageModule)}
+  { path: 'login', loadChildren: () => import('./log_in/logIn.module').then(m => m.LogInPageModule)}
 ];
 
 @NgModule({
