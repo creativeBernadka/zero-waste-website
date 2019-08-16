4e52be940c5f84855fe378026e179865740acffa
creativeBernadka <bfryczkowska@gmail.com>
2019-08-16 09:43:24 +0200

    ADD: user module and component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'login', loadChildren: () => import('./log_in/logIn.module').then(m => m.LogInPageModule)}
+  { path: 'login', loadChildren: () => import('./log_in/logIn.module').then(m => m.LogInPageModule)},
+  { path: 'home/user', loadChildren: () => import('./user_overview/user.module').then( m => m.UserPageModule)}
 ];
 
 @NgModule({
---
+++ src/app/user_overview/user.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: 'user.page.html',
+    styleUrls: ['user.page.scss'],
+})
+export class UserPage {
+
+    constructor() {}
+
+}
