04bc00f832ca41038dd6ffd15916c3bf8cd54313
creativeBernadka <bfryczkowska@gmail.com>
2019-08-22 14:07:19 +0200

    ADD: logout button functionality added

--- src/app/user_overview/user.page.ts
+++ src/app/user_overview/user.page.ts
@@ -1,20 +1,35 @@
 import { Component } from '@angular/core';
+import {AuthService} from '../../services/authentication.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'user.page.html',
     styleUrls: ['user.page.scss'],
+    providers: [AuthService]
 })
 export class UserPage {
 
-    constructor() {}
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {}
 
     shops = [{
-        name: 'Shop 1'
+        name: 'Shop 1',
+        typeOfPlace: 'sklep'
     }, {
-        name: 'Shop 2'
+        name: 'Shop 2',
+        typeOfPlace: 'punkt naprawczy'
     }, {
-        name: 'Shop 3'
+        name: 'Shop 3',
+        typeOfPlace: 'restauracja'
     }];
+
+    logOut() {
+        this.authService.logout();
+        console.log('User is logged out');
+        this.router.navigateByUrl('home');
+    }
 
 }
